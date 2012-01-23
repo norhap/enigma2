772da7a93d6576667d3d5dc0c628634cf001ac94
Andy Blackburn <andyblac@o2.co.uk>
2012-01-23 14:07:07 +0000

    Group logs together and make both debug and crash log location user definable

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -925,6 +925,27 @@ def InitUsageConfig():
 	config.crash.pystackonspinner = ConfigYesNo(default = False)
 	config.crash.pystackonspinner.addNotifier(updateStackTracePrinter, immediate_feedback = False, initial_call = True)
 
+	# config.crash.logtimeformat sets ENIGMA_DEBUG_TIME environmental variable on enigma2 start from enigma2.sh
+	config.crash.logtimeformat = ConfigSelection(default = "1", choices = [
+		("0", _("None")),
+		("1", _("Boot time")),
+		("2", _("Local time")),
+		("3", _("Boot time and local time")) ])
+	config.crash.logtimeformat.save_forced = True
+
+	debugpath = [('/home/root/logs/', '/home/root/')]
+	for p in harddiskmanager.getMountedPartitions():
+		if os.path.exists(p.mountpoint):
+			d = os.path.normpath(p.mountpoint)
+			if p.mountpoint != '/':
+				debugpath.append((p.mountpoint + 'logs/', d))
+	config.crash.debug_path = ConfigSelection(default = "/home/root/logs/", choices = debugpath)
+
+	def updatedebug_path(configElement):
+		if not os.path.exists(config.crash.debug_path.value):
+			os.mkdir(config.crash.debug_path.value,0755)
+	config.crash.debug_path.addNotifier(updatedebug_path, immediate_feedback = False)
+
 	config.seek = ConfigSubsection()
 	config.seek.selfdefined_13 = ConfigNumber(default=15)
 	config.seek.selfdefined_46 = ConfigNumber(default=60)
