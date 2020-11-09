62a0a31c1cecbc8d0dbb1ecbac84aa8f7a6394a0
norhap <elfogondesegovia@gmail.com>
2020-11-09 16:48:09 +0100

    Update epgcache.cpp

--- lib/dvb/epgcache.cpp
+++ lib/dvb/epgcache.cpp
@@ -1047,7 +1047,7 @@ next:
 				}
 			}
 			{
-				CFile f("/media/hdd/time_map", "w+");
+				CFile f("/media/hdd/time_map.txt", "w+");
 				int i = 0;
 				for (timeMap::iterator it(servicemap.byTime.begin()); it != servicemap.byTime.end(); ++it )
 				{
