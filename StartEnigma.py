22546dd97525a90a2b3c7a5dc69f41217f5f4eb4
fogoegami <elfogondesegovia@gmail.com>
2021-03-02 12:29:04 +0100

    Get out of stone age: Change mytest.py to StartEnigma.py

--- StartEnigma.py
+++ StartEnigma.py
@@ -110,7 +110,7 @@ try:
 	def runReactor():
 		reactor.run(installSignalHandlers=False)
 except ImportError as e:
-	print("[mytest] twisted not available")
+	print("[StartEnigma] Twisted not available")
 
 	def runReactor():
 		enigma.runMainloop()
@@ -217,7 +217,7 @@ class Session:
 			try:
 				p.__call__(reason=0, session=self)
 			except:
-				print("[mytest] Plugin raised exception at WHERE_SESSIONSTART")
+				print("[StartEnigma] Plugin raised exception at WHERE_SESSIONSTART")
 				import traceback
 				traceback.print_exc()
 
@@ -339,7 +339,7 @@ class Session:
 
 	def close(self, screen, *retval):
 		if not self.in_exec:
-			print("[mytest] close after exec!")
+			print("[StartEnigma] Close after exec!")
 			return
 
 		# be sure that the close is for the right dialog!
@@ -391,7 +391,7 @@ class PowerKey:
 		globalActionMap.actions["discrete_off"] = self.standby
 
 	def shutdown(self):
-		print("[mytest] PowerOff - Now!")
+		print("[StartEnigma] Power off, now!")
 		if not Screens.Standby.inTryQuitMainloop and self.session.current_dialog and self.session.current_dialog.ALLOW_SUSPEND:
 			self.session.open(Screens.Standby.TryQuitMainloop, 1)
 		else:
@@ -417,7 +417,7 @@ class PowerKey:
 					exec("from " + selected[1] + " import *")
 					exec("self.session.open(" + ",".join(selected[2:]) + ")")
 				except:
-					print("[mytest] error during executing module %s, screen %s" % (selected[1], selected[2]))
+					print("[StartEnigma] Error during executing module %s, screen %s" % (selected[1], selected[2]))
 			elif selected[0] == "Menu":
 				root = mdom.getroot()
 				for x in root.findall("menu"):
@@ -561,9 +561,9 @@ def runScreenTest():
 			else:
 				wptime = startTime[0] - 240
 		if model.startswith == "gb":
-			print("[mytest] dvb time sync disabled... so set RTC now to current linux time!", strftime("%Y/%m/%d %H:%M", localtime(nowTime)))
+			print("[StartEnigma] dvb time sync disabled... so set RTC now to current linux time!", strftime("%Y/%m/%d %H:%M", localtime(nowTime)))
 			setRTCtime(nowTime)
-		print("[mytest] set wakeup time to", strftime("%Y/%m/%d %H:%M", localtime(wptime)))
+		print("[StartEnigma] Set wakeup time to", strftime("%Y/%m/%d %H:%M", localtime(wptime)))
 		setFPWakeuptime(wptime)
 		config.misc.prev_wakeup_time.value = int(startTime[0])
 		config.misc.prev_wakeup_time_type.value = startTime[1]
