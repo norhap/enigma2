958388bb7ec9a028ca19a529535f1be99d5c3a18
fogoegami <elfogondesegovia@gmail.com>
2021-03-09 20:21:43 +0100

    Add getDisplayType

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -80,6 +80,9 @@ class About(Screen):
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
 		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
+		AboutText += "\n"
+		if not boxbranding.getDisplayType().startswith(' '):
+			AboutText += _("Display tipo: ") + boxbranding.getDisplayType() + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
