9eaf4ad5f90bbdcca68667bd58cdf9a187d45471
norhap <elfogondesegovia@gmail.com>
2021-05-31 15:15:25 +0200

    SoftcamSetup.py: not show info for wicardd

--- lib/python/Screens/SoftcamSetup.py
+++ lib/python/Screens/SoftcamSetup.py
@@ -79,7 +79,7 @@ class SoftcamSetup(Setup):
 		else:
 			self["key_yellow"].setText("")
 			self["restartActions"].setEnabled(False)
-		if self["config"].getCurrent()[1] == config.misc.softcams and config.misc.softcams.value and config.misc.softcams.value.lower() != "none":
+		if self["config"].getCurrent()[1] == config.misc.softcams and config.misc.softcams.value and config.misc.softcams.value.lower() != "none" and config.misc.softcams.value.lower() != "wicardd":
 			self["key_blue"].setText(_("Info"))
 			self["infoActions"].setEnabled(True)
 		else:
