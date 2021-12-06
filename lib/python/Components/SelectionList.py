0aa97610bfd8364938a7a86482aa8deb8ff0cf96
norhap <elfogondesegovia@gmail.com>
2021-12-06 01:32:25 +0100

    SelectionList.py: no use BT_SCALE | BT_KEEP_ASPECT_RATIO on Pixmap

--- lib/python/Components/SelectionList.py
+++ lib/python/Components/SelectionList.py
@@ -1,4 +1,4 @@
-from enigma import BT_KEEP_ASPECT_RATIO, BT_SCALE, RT_HALIGN_LEFT, RT_VALIGN_CENTER, eListboxPythonMultiContent, gFont
+from enigma import RT_HALIGN_LEFT, RT_VALIGN_CENTER, eListboxPythonMultiContent, gFont
 
 from skin import fonts, parameters
 from Components.MenuList import MenuList
@@ -14,13 +14,10 @@ def SelectionEntryComponent(description, value, index, selected):
 		(description, value, index, selected),
 		(eListboxPythonMultiContent.TYPE_TEXT, dx, dy, dw, dh, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, description)
 	]
-	icons = [
-		LoadPixmap(path=resolveFilename(SCOPE_GUISKIN, "icons/lock_on.png")),
-		LoadPixmap(path=resolveFilename(SCOPE_GUISKIN, "icons/lock_off.png"))
-	]
+	global icons
 	icon = icons[0] if selected else icons[1]
 	ix, iy, iw, ih = parameters.get("SelectionListLock", (10, 2, 26, 26))
-	res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, ix, iy, iw, ih, icon, BT_SCALE | BT_KEEP_ASPECT_RATIO))
+	res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, ix, iy, iw, ih, icon, None, None))
 	return res
 
 
@@ -30,6 +27,11 @@ class SelectionList(MenuList):
 		font = fonts.get("SelectionList", ("Regular", 25, 30))
 		self.l.setFont(0, gFont(font[0], font[1]))
 		self.l.setItemHeight(font[2])
+		global icons
+		icons = [
+			LoadPixmap(path=resolveFilename(SCOPE_GUISKIN, "icons/lock_on.png")),
+			LoadPixmap(path=resolveFilename(SCOPE_GUISKIN, "icons/lock_off.png"))
+		]
 
 	def addSelection(self, description, value, index, selected=True):
 		self.list.append(SelectionEntryComponent(description, value, index, selected))
