8c4ba56f220be7714ee016f08d0586b7447b421b
norhap <elfogondesegovia@gmail.com>
2022-05-08 02:31:41 +0200

    ChannelsImporter.py: set timeout message to 10

--- lib/python/Components/ChannelsImporter.py
+++ lib/python/Components/ChannelsImporter.py
@@ -278,7 +278,7 @@ class ChannelsImporter():
 			self.importEPGCallback()
 		else:
 			print("[ChannelsImporter] checkEPGCallback Download epg.dat from remote receiver failed. Check file exists on remote receiver.")
-			AddNotificationWithID("ChannelsImportNOK", MessageBox, _("EPG on server is not in internal flash or manual IP not set."), type=MessageBox.TYPE_ERROR, timeout=5)
+			AddNotificationWithID("ChannelsImportNOK", MessageBox, _("EPG not imported check that path EPG in server is internal flash\nCheck IP in fallback tuner settings was manually assigned"), type=MessageBox.TYPE_ERROR, timeout=10)
 
 	def importEPGCallback(self):
 		print("[ChannelsImporter] importEPGCallback '%s%s' downloaded successfully. " % (self.remoteEPGpath, self.remoteEPGfile))
