c38e3357e71fd7eb22db901dfac360f5fd14d7e4
norhap <elfogondesegovia@gmail.com>
2022-06-16 22:02:02 +0200

    PositionerSetup: StringIO object has no attribute 'reset' use seek

--- lib/python/Plugins/SystemPlugins/PositionerSetup/log.py
+++ lib/python/Plugins/SystemPlugins/PositionerSetup/log.py
@@ -33,7 +33,7 @@ def write(data):
 	try:
 		if logfile.tell() > size:
 			# Do a sort of 16k round robin
-			logfile.reset()
+			logfile.seek(0)
 		logfile.write(data)
 	finally:
 		mutex.release()
@@ -46,7 +46,7 @@ def getvalue():
 	try:
 		pos = logfile.tell()
 		head = logfile.read()
-		logfile.reset()
+		logfile.seek(0)
 		tail = logfile.read(pos)
 	finally:
 		mutex.release()
