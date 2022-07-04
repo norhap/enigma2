0c9961759dd61a087d254d06b49877938dc016cc
norhap <elfogondesegovia@gmail.com>
2022-07-04 02:39:10 +0200

    LanguageSelection.py: show button only with set text
    delete language, show language to be deleted

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -54,8 +54,8 @@ class LanguageSelection(Screen):
 		self.updateList()
 		self.onLayoutFinish.append(self.selectActiveLanguage)
 
-		self["key_red"] = Label("")
-		self["key_green"] = Label("")
+		self["key_red"] = StaticText("")
+		self["key_green"] = StaticText("")
 		self["key_yellow"] = Label(_("Add Language"))
 		self["key_blue"] = Label(_("Delete Language(s)"))
 		self["description"] = Label(_("'Save' changes active language.\n'Add Language' or MENU adds additional language(s).\n'Delete Language' allows either deletion of all but English and active language OR selected language."))
@@ -130,7 +130,7 @@ class LanguageSelection(Screen):
 			if curlang == t[0]:
 				lang = t[1]
 				break
-		self.session.openWithCallback(self.delLangCB, MessageBox, _("Select 'Yes' to delete all languages except English and current language:\n\nSelect 'No' to delete only the chosen language:\n\n") + _("%s") % (lang), default=True)
+		self.session.openWithCallback(self.delLangCB, MessageBox, _("Select 'Yes' to delete all languages except English and current language:\n\nSelect 'No' to delete only the chosen language:\n\n") + _("%s") % (lang), default=config.osd.language.value)
 
 	def delLangCB(self, answer):
 		if answer:
