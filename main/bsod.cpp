fa77ef3cf42b5fc5f51d55818333b33b03eb9974
norhap <elfogondesegovia@gmail.com>
2022-12-27 20:42:41 +0100

    [bsod]
    * add close in stringFromFile
    * use dumpFile for nimsockets
    * use enigma.info for header data
    
    Co-Authored-By: jbleyel <jbleyel@gmx.net>

--- main/bsod.cpp
+++ main/bsod.cpp
@@ -3,6 +3,7 @@
 #include <csignal>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 #ifdef __GLIBC__
 #include <execinfo.h>
 #endif
@@ -75,6 +76,22 @@ static void stringFromFile(FILE* f, const char* context, const char* filename)
 		std::string line;
 		std::getline(in, line);
 		fprintf(f, "%s=%s\n", context, line.c_str());
+		in.close();
+	}
+}
+
+static void dumpFile(FILE* f, const char* filename)
+{
+	std::ifstream in(filename);
+	if (in.good()) {
+		do
+		{
+			std::string line;
+			std::getline(in, line);
+			fprintf(f, "%s\n", line.c_str());
+		}
+		while (in.good());
+		in.close();
 	}
 }
 
@@ -161,8 +178,34 @@ void bsodFatal(const char *component)
 			enigma2_rev,
 			component);
 
+		std::ifstream in(eEnv::resolve("${libdir}/enigma.info").c_str());
+		const std::list<std::string> enigmainfovalues {
+			"model="
+		};
+
+		if (in.good()) {
+			do
+			{
+				std::string line;
+				std::getline(in, line);
+				for(std::list<std::string>::const_iterator i = enigmainfovalues.begin(); i != enigmainfovalues.end(); ++i)
+				{
+					if (line.find(i->c_str()) != std::string::npos) {
+						line.erase(std::remove( line.begin(), line.end(), '\"' ),line.end());
+						line.erase(std::remove( line.begin(), line.end(), '\'' ),line.end());
+						fprintf(f, "%s\n", line.c_str());
+						break;
+					}
+				}
+			}
+			while (in.good());
+			in.close();
+		}
+
+		fprintf(f, "\n");
 		stringFromFile(f, "kernelcmdline", "/proc/cmdline");
-		stringFromFile(f, "nimsockets", "/proc/bus/nim_sockets");
+		fprintf(f, "\nnimsockets:\n");
+		dumpFile(f, "/proc/bus/nim_sockets");
 
 		/* dump the log ringbuffer */
 		fprintf(f, "\n\n");
