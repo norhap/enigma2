5fede92d6a5994040cd50430f797b5c6139767f3
norhap <elfogondesegovia@gmail.com>
2023-03-19 01:24:53 +0100

    ntpdate: move root file check to module Time
    - Do not check at every reboot -restart enigma2.
    - Only check file if we change the synchronized time value.
    - If we change the synchronized time value we create the structure in real time.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1,6 +1,6 @@
 from locale import AM_STR, PM_STR, nl_langinfo
 from os import makedirs, remove
-from os.path import exists, isfile, islink, join as pathjoin, normpath
+from os.path import exists, isfile, join as pathjoin, normpath
 from time import mktime
 
 from enigma import eBackgroundFileEraser, eDVBDB, eEnv, setEnableTtCachingOnOff, setPreferredTuner, setSpinnerOnOff, setTunerTypePriorityOrder, Misc_Options, eServiceEvent, eDVBLocalTimeHandler, eEPGCache, getBoxType
@@ -1658,15 +1658,9 @@ def InitUsageConfig():
 		if configElement.value == "dvb" or not GetIPsFromNetworkInterfaces():
 			eDVBLocalTimeHandler.getInstance().setUseDVBTime(True)
 			eEPGCache.getInstance().timeUpdated()
-			if configElement.value == "dvb" and islink("/etc/network/if-up.d/ntpdate-sync"):
-				Console().ePopen("sed -i '/ntpdate-sync/d' /etc/cron/crontabs/root;unlink /etc/network/if-up.d/ntpdate-sync")
 		else:
 			eDVBLocalTimeHandler.getInstance().setUseDVBTime(False)
 			eEPGCache.getInstance().timeUpdated()
-			if not islink("/etc/network/if-up.d/ntpdate-sync"):
-				Console().ePopen("ln -s /usr/bin/ntpdate-sync /etc/network/if-up.d/ntpdate-sync")
-			if not isfile("/var/spool/cron/root"):
-				Console().ePopen("echo '30 * * * * /usr/bin/ntpdate-sync silent' >>/var/spool/cron/root")
 	config.ntp.timesync = ConfigSelection(default="auto", choices=[
 		("auto", _("Auto")),
 		("dvb", _("Transponder time")),
