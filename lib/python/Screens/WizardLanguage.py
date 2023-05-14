59f1f5f121c95a51dd35ea8401096800f4ab2351
norhap <elfogondesegovia@gmail.com>
2023-05-14 18:45:53 +0200

    WizardLanguage.py: change language in wizard only with OSD default
    continue to set the default language and time settings from user:
    
    https://github.com/norhap/enigma2-openvision-1/commit/2bc06643968d55a7162f9631758e1ab60845489a

--- lib/python/Screens/WizardLanguage.py
+++ lib/python/Screens/WizardLanguage.py
@@ -1,13 +1,28 @@
+from Tools.Geolocation import geolocation
 from Components.Label import Label
 from Screens.LanguageSelection import LanguageWizard
 from Screens.Wizard import Wizard
+from Components.config import config, ConfigBoolean
+
+config.misc.firstrun = ConfigBoolean(default=True)
+geolocationData = geolocation.getGeolocationData(fields="isp,org,mobile,proxy,query", useCache=False)
 
 
 class WizardLanguage(Wizard):
-	def __init__(self, session, showSteps=True, showStepSlider=True, showList=True, showConfig=True):
-		Wizard.__init__(self, session, showSteps, showStepSlider, showList, showConfig)
-		self["key_red"] = Label()
-		self["languagetext"] = Label(_("Change Language"))
+	if geolocationData.get("status", None) != "success" and config.misc.firstrun.value:
+		def __init__(self, session, showSteps=True, showStepSlider=True, showList=True, showConfig=True):
+			Wizard.__init__(self, session, showSteps, showStepSlider, showList, showConfig)
+			self["key_red"] = Label()
+			self["languagetext"] = Label(_("Change Language"))
 
-	def red(self):
-		self.session.open(LanguageWizard)
+		def red(self):
+			self.session.open(LanguageWizard)
+
+	if not config.misc.firstrun.value:
+		def __init__(self, session, showSteps=True, showStepSlider=True, showList=True, showConfig=True):
+			Wizard.__init__(self, session, showSteps, showStepSlider, showList, showConfig)
+			self["key_red"] = Label()
+			self["languagetext"] = Label(_("Change Language"))
+
+		def red(self):
+			self.session.open(LanguageWizard)
