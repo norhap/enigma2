4a094635d22646b8e20bfc17a832c7ed5e39a75f
Captain <captain.onboard@web.de>
2023-07-18 23:10:07 +0200

    pdate useragent, to fix streams from provider cdn

--- lib/python/Tools/Downloader.py
+++ lib/python/Tools/Downloader.py
@@ -3,14 +3,13 @@ from os import unlink
 import requests
 from twisted.internet import reactor
 from urllib.request import urlopen, Request
-from Components.SystemInfo import BRAND, DISPLAYMODEL
 
 
 class DownloadWithProgress:
 	def __init__(self, url, outputFile, *args, **kwargs):
 		self.url = url
 		self.outputFile = outputFile
-		self.userAgent = "%s %s Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenVision;;;)" % (BRAND, DISPLAYMODEL)
+		self.userAgent = "HbbTV/1.1.1 (+PVR+RTSP+DL; Sonic; TV44; 1.32.455; 2.002) Bee/3.5"
 		# self.agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5"
 		self.totalSize = 0
 		self.progress = 0
