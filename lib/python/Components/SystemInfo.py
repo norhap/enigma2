d7e43d4d400ca5faff6fb52a632b3c4c9a4a26dd
norhap <elfogondesegovia@gmail.com>
2023-10-26 18:58:40 +0200

    StreamRelay: prepare for more streamrelay cam
    run code only if stream relay API exists
    follow https://github.com/norhap/enigma2/commit/dfb50ee6f1a2caa9f06038cc640e92fcbdf4d5e8

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -5,6 +5,7 @@ from re import findall
 from boxbranding import getMachineName
 from enigma import Misc_Options, eAVControl, eDVBCIInterfaces, eDVBResourceManager, eGetEnigmaDebugLvl, getPlatform, eDBoxLCD
 
+from process import ProcessList
 from Tools.Directories import SCOPE_SKINS, SCOPE_LIBDIR, scopeLCDSkin, fileCheck, fileExists, fileHas, fileReadLines, pathExists, resolveFilename
 from Tools.StbHardware import getWakeOnLANType
 
@@ -189,9 +190,10 @@ BoxInfo.setItem("RemoteDelay", 700)
 BoxInfo.setItem("have24hz", eAVControl.getInstance().has24hz())
 BoxInfo.setItem("hashdmiin", BoxInfo.getItem("hdmifhdin") or BoxInfo.getItem("hdmihdin"))
 
-from process import ProcessList  # items availables for streamrelay
-ICam = str(ProcessList().named("oscam-emu")).strip("[]")
-BoxInfo.setItem("ICam", ICam)
+API_STREAMRELAY = ["oscam-emu",]  # add more cams
+for cam in API_STREAMRELAY:
+	streamrelay = str(ProcessList().named(cam)).strip("[]")
+	BoxInfo.setItem("StreamRelay", streamrelay)  # items availables for streamrelay
 
 SystemInfo["InDebugMode"] = eGetEnigmaDebugLvl() >= 4
 SystemInfo["CommonInterface"] = MODEL in ("h9combo", "h9combose", "h10", "pulse4kmini") and 1 or eDVBCIInterfaces.getInstance().getNumOfSlots()
