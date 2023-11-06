46eef9c67997d21e80574fb14d69c2711002f73c
norhap <elfogondesegovia@gmail.com>
2023-11-06 17:15:11 +0100

    show PiP enable in MiniTV if box with features

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2663,15 +2663,19 @@ class InfoBarPiP:
 
 	def getShowHideName(self):
 		if self.session.pipshown:
-			if SystemInfo["LcdLiveTV"] and config.plugins.minitv.enable.value == "enable":
-				return _("Disable Picture in Picture - Disable in LCD MiniTV")
+			if SystemInfo["LcdLiveTV"] and isPluginInstalled("MiniTV"):
+				if config.plugins.minitv.enable.value == "enable":
+					return _("Disable Picture in Picture - Disable in LCD MiniTV")
+				else:
+					return _("Disable Picture in Picture")
 			else:
 				return _("Disable Picture in Picture")
 		else:
-			if SystemInfo["LcdLiveTV"] and config.plugins.minitv.enable.value == "disable":
-				return _("Activate Picture in Picture")
-			elif SystemInfo["LcdLiveTV"]:
-				return _("Activate Picture in Picture - Enable in LCD MiniTV")
+			if SystemInfo["LcdLiveTV"] and isPluginInstalled("MiniTV"):
+				if config.plugins.minitv.enable.value == "disable":
+					return _("Activate Picture in Picture")
+				else:
+					return _("Activate Picture in Picture - Enable in LCD MiniTV")
 			else:
 				return _("Activate Picture in Picture")
 
