8266c6cba1863b896ed5dfaf78e6956147dd61fc
norhap <elfogondesegovia@gmail.com>
2023-12-05 19:49:18 +0100

    MessageBox.py add mandatoryWidgets

--- lib/python/Screens/MessageBox.py
+++ lib/python/Screens/MessageBox.py
@@ -13,8 +13,9 @@ class MessageBox(Screen, HelpableScreen):
 	skin = """
 	<screen name="MessageBox" position="center,center" size="520,225" resolution="1280,720">
 		<widget name="icon" pixmaps="icons/input_question.png,icons/input_info.png,icons/input_warning.png,icons/input_error.png,icons/input_message.png" position="10,10" size="53,53" alphaTest="blend" conditional="icon" scale="1" transparent="1" />
-		<widget name="text" position="75,10" size="435,55" font="Regular;22" transparent="1" />
-		<widget name="list" position="10,75" size="500,140" conditional="list" enableWrapAround="1" font="Regular;25" itemHeight="35" scrollbarMode="showOnDemand" transparent="1" />
+		<widget name="text" position="75,10" size="435,160" font="Regular;20" transparent="1" />
+		<widget name="list" position="10,173" size="500,50" conditional="list" enableWrapAround="1" font="Regular;20" itemHeight="23" scrollbarMode="showOnDemand" transparent="1" />
+		<widget name="autoresize" conditional="autoresize" position="75,10" size="435,160" font="Regular;20" transparent="1"/>
 	</screen>"""
 
 	TYPE_NOICON = 0
@@ -25,7 +26,7 @@ class MessageBox(Screen, HelpableScreen):
 	TYPE_MESSAGE = 5
 
 	def __init__(self, session, text, type=TYPE_YESNO, timeout=-1, list=None, default=True, closeOnAnyKey=False, enableInput=True, msgBoxID=None, typeIcon=None, timeoutDefault=None, windowTitle=None, skinName=None, close_on_any_key=False, enable_input=True, timeout_default=None, title=None, picon=None, skin_name=None, simple=None):
-		Screen.__init__(self, session)
+		Screen.__init__(self, session, mandatoryWidgets=["autoresize", "icon", "list", "text"])
 		HelpableScreen.__init__(self)
 		self.timerRunning = False
 		self["autoresize"] = Label("")
