341e567e8e3bb7f8c37efcdde17a650f5324595e
IanSav <IanSav@users.noreply.github.com>
2024-04-27 16:12:40 +1000

    [EventInfo.py] Ensure that times are an integer
    Enigma2 times used for displays, and most processing, are in whole seconds.

--- lib/python/Components/Sources/EventInfo.py
+++ lib/python/Components/Sources/EventInfo.py
@@ -20,7 +20,7 @@ class pServiceEvent(object):
 		self.m_ShortDescriptionNext = ""
 		self.m_ExtendedDescriptionNow = ""
 		self.m_ExtendedDescriptionNext = ""
-		self.m_Begin = time()
+		self.m_Begin = int(time())
 		self.m_Duration = 0
 
 		sTagTitle = info.getInfoString(iServiceInformation.sTagTitle)
