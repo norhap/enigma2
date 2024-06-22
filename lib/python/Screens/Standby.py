1dd85a2ae79286f15e2c4681737c0590adcb9988
norhap <elfogondesegovia@gmail.com>
2024-06-22 23:05:09 +0200

    [Standby] goto standby - always stop sleeptimer
    Co-Authored-By: Dimitrij <Dima-73@inbox.lv>

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -175,6 +175,8 @@ class StandbyScreen(Screen):
 			del self.session.pip
 		self.session.pipshown = False
 
+		self.infoBarInstance and hasattr(self.infoBarInstance, "sleepTimer") and self.infoBarInstance.sleepTimer.stop()
+
 		if BoxInfo.getItem("scart"):
 			self.avswitch.setInput("SCART")
 		else:
