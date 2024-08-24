74d24d0c046398022955f8b54a56912b49e28788
DimitarCC <dimitar_tsenev@outlook.com>
2024-08-24 16:52:38 +0300

    [Removed] Obsolete options for CA handling [Added] Configuration to free CA device on program remove (for selected boxes)

--- lib/dvb_ci/dvbci_ccmgr.cpp
+++ lib/dvb_ci/dvbci_ccmgr.cpp
@@ -141,6 +141,10 @@ void eDVBCICcSession::removeProgram(uint16_t program_number, std::vector<uint16_
 
 	for (std::vector<uint16_t>::iterator it = pids.begin(); it != pids.end(); ++it)
 		descrambler_set_pid(m_descrambler_fd, m_slot->getSlotID(), 0, *it);
+
+#ifdef USE_ALTERNATE_CA_HANDLING
+	descrambler_deinit(m_descrambler_fd);
+#endif
 }
 
 void eDVBCICcSession::cc_open_req()
@@ -784,13 +788,13 @@ void eDVBCICcSession::set_descrambler_key()
 	if (m_descrambler_fd != -1 && m_current_ca_demux_id != m_slot->getCADemuxID())
 	{
 		descrambler_deinit(m_descrambler_fd);
-		m_descrambler_fd = descrambler_init(m_slot->getSlotID(), m_slot->getCADemuxID() + (m_slot->getIsCA0Excluded() ? 1 : 0));
+		m_descrambler_fd = descrambler_init(m_slot->getSlotID(), m_slot->getCADemuxID());
 		m_current_ca_demux_id = m_slot->getCADemuxID();
 	}
 
 	if (m_descrambler_fd == -1 && m_slot->getCADemuxID() > -1)
 	{
-		m_descrambler_fd = descrambler_init(m_slot->getSlotID(), m_slot->getCADemuxID() + (m_slot->getIsCA0Excluded() ? 1 : 0));
+		m_descrambler_fd = descrambler_init(m_slot->getSlotID(), m_slot->getCADemuxID());
 		m_current_ca_demux_id = m_slot->getCADemuxID();
 	}
 
