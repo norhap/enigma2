ae09574c69a581f2e49fff93bd1a49fb28d01b35
norhap <elfogondesegovia@gmail.com>
2024-08-24 20:38:09 +0200

    ActionMap.py: Do not assign a lambda expression, use a def (E731)
    skin_fallback_1080: deactived render xtraPoster

--- lib/python/Components/ActionMap.py
+++ lib/python/Components/ActionMap.py
@@ -69,13 +69,14 @@ def parseKeymap(filename, context, actionMapInstance, device, domKeys):
 			print("[ActionMap] Error: Attribute 'flag' in context '%s' id '%s' (%d) in file '%s' must be specified!" % (context, keyName, keyId, filename))
 			error = True
 		else:
-			flagToValue = lambda x: {
-				"m": 1,
-				"b": 2,
-				"r": 4,
-				"l": 8,
-				"s": 32
-			}[x]
+			def flagToValue(x):
+				return {
+					"m": 1,
+					"b": 2,
+					"r": 4,
+					"l": 8,
+					"s": 32
+				}[x]
 			newFlags = sum(map(flagToValue, flags))
 			if not newFlags:
 				print("[ActionMap] Error: Attribute 'flag' value '%s' in context '%s' id '%s' (%d) in file '%s' appears invalid!" % (flags, context, keyName, keyId, filename))
