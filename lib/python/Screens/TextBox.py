aeced892c44d03832905ec2304ab6db7e2adc3ba
norhap <elfogondesegovia@gmail.com>
2024-10-28 23:36:54 +0100

    TexBox.py: sync with OpenViX

--- lib/python/Screens/TextBox.py
+++ lib/python/Screens/TextBox.py
@@ -4,19 +4,26 @@ from Components.ScrollLabel import ScrollLabel
 
 
 class TextBox(Screen):
-	def __init__(self, session, text="", title=None, pigless=False):
+	def __init__(self, session, text="", title=None, skin_name=None, label=None):
 		Screen.__init__(self, session)
-		if pigless:
-			self.skinName = ["TextBoxPigLess", "TextBox"]
+		if isinstance(self.skinName, str):
+			self.skinName = [self.skinName]
+		if "TextBox" not in self.skinName:
+			self.skinName.append("TextBox")
+		if isinstance(skin_name, str):
+			self.skinName.insert(0, skin_name)
 		self.text = text
-		self["text"] = ScrollLabel(self.text)
+		self.label = "text"
+		if isinstance(label, str):
+			self.label = label
+		self[self.label] = ScrollLabel(self.text)
 
 		self["actions"] = ActionMap(["OkCancelActions", "NavigationActions"],
 			{
 			"cancel": self.close,
 			"ok": self.close,
-			"up": self["text"].pageUp,
-			"down": self["text"].pageDown,
+			"up": self[self.label].pageUp,
+			"down": self[self.label].pageDown,
 		}, -1)
 
 		if title:
