5c29af3dd223b5ce3d035a9aa545c59a65413b6b
Littlesat <littlesat@openpli.org>
2024-11-09 10:40:34 +0100

    Arrange when go left to get through the ask_time limit line
    Previously when you select an event e.g. in single or in multi EPG but
    also when you go to primetime there is a hard line at the left from the
    GMEPG so you cannnot scroll left back to 'now'. This commit opens that
    barrier so you can always scroll left back to now.
    
    Might still have some finetuning needed as it does not go always to the
    previous event (but on the first one of the 'just' created new case. But
    it is already better as it e.g. resolved the following scenario:
    
    Open the GMEPG:
    
    - select a random programnme
    - Press the EPG INFO button
    - Press Yellow to open the single EPG for the selected channel
    - Scroll down a bit
    - Press Exit
    
    You return the the GMPEG, but you can't scroll back anymore to before the progamme you selected in the single EPG
    
    https://forums.openpli.org/topic/16424-some-questions-bugs/page-353

--- lib/python/Plugins/Extensions/GraphMultiEPG/GraphMultiEpg.py
+++ lib/python/Plugins/Extensions/GraphMultiEPG/GraphMultiEpg.py
@@ -665,7 +665,14 @@ class EPGList(GUIComponent):
 					self.offs -= 1
 					self.fillMultiEPG(None)  # refill
 					return True
-			elif dir == +2:  # next page
+				else:
+					new_time = self.time_base - self.time_epoch * 60
+					now = time()
+					if new_time - now + self.time_epoch < 0:
+						new_time = now - now % int(config.misc.graph_mepg.roundTo.getValue())
+					self.fillMultiEPG(None, stime=new_time)
+					return True
+			elif dir == +2: #next page
 				self.offs += 1
 				self.fillMultiEPG(None)  # refill
 				return True
