ba4acd8ca7874d0f626ec61058509d82a489defc
jbleyel <jbleyel@gmx.net>
2025-02-11 12:12:17 +0100

    [eListboxPythonServiceContent] * add missing getList

--- lib/service/elistboxservicecontent.h
+++ lib/service/elistboxservicecontent.h
@@ -60,6 +60,7 @@ public:
 	void getCurrent(eServiceReference &ref);
 	void getPrev(eServiceReference &ref);
 	void getNext(eServiceReference &ref);
+	PyObject *getList();
 
 	int getNextBeginningWithChar(char c);
 	int getPrevMarkerPos();
