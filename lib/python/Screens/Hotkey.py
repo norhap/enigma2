816bcb4b87e30bf6d1ed4e8e7771b36ddc567451
norhap <elfogondesegovia@gmail.com>
2025-12-07 09:57:00 +0100

    Hotkey.py: Add fallback tuner to Setup

--- lib/python/Screens/Hotkey.py
+++ lib/python/Screens/Hotkey.py
@@ -233,6 +233,7 @@ def getHotkeyFunctions():
 	hotkey.functions.append((_("Software update"), "Module/Screens.SoftwareUpdate/UpdatePlugin", "Setup"))
 	hotkey.functions.append((_("Latest Commits"), "Module/Screens.About/CommitInfo", "Setup"))
 	hotkey.functions.append((_("CI (Common Interface) Setup"), "Module/Screens.Ci/CiSelection", "Setup"))
+	hotkey.functions.append((_("Fallback remote receiver setup"), "Module/Screens.SetupFallbacktuner/SetupFallbacktuner", "Setup"))
 	hotkey.functions.append((_("Tuner Configuration"), "Module/Screens.Satconfig/NimSelection", "Scanning"))
 	hotkey.functions.append((_("Manual Scan"), "Module/Screens.ScanSetup/ScanSetup", "Scanning"))
 	hotkey.functions.append((_("Automatic Scan"), "Module/Screens.ScanSetup/ScanSimple", "Scanning"))
