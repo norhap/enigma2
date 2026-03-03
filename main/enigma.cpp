7a41fcf372f1863eadd9697f1f14b728353fdd86
jbleyel <jbleyel@gmx.net>
2026-03-03 17:06:59 +0100

    add flag for native cuts support

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -467,7 +467,7 @@ const char *getPlatform()
 
 int getE2Flags()
 {
-	return 3; // start/stop Audio = 1 | WebP = 2
+	return 7; // start/stop Audio = 1 | WebP = 2 | NativeCutFileSupport = 4
 }
 
 bool checkLogin(const char *user, const char *password)
