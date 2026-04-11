53686f89dd4e803959eb184d7acb1c5a574eed06
norhap <elfogondesegovia@gmail.com>
2026-04-11 22:16:30 +0200

    [ServiceReference] resolveAlternate - add sanity check
    Co-Authored-By: Dimitrij <Dima-73@inbox.lv>

--- lib/python/ServiceReference.py
+++ lib/python/ServiceReference.py
@@ -69,7 +69,7 @@ def isPlayableForCur(ref):
 
 def resolveAlternate(ref):
 	nref = None
-	if ref.flags & eServiceReference.isGroup:
+	if ref and ref.flags & eServiceReference.isGroup:
 		nref = getBestPlayableServiceReference(ref, getPlayingref(ref))
 		if not nref:
 			nref = getBestPlayableServiceReference(ref, eServiceReference(), True)
