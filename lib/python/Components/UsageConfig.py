0a6e0dad60f3cf962b15d4a5ce9f436fcf0b9fbe
jbleyel <jbleyel@gmx.net>
2026-05-16 05:54:28 +0200

    Merge pull request #3768 from WXbet/master
    SoftCSA Fix pack

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1951,7 +1951,8 @@ def InitUsageConfig():
 	config.softcsa = ConfigSubsection()
 	config.softcsa.decoderRelease = ConfigSelection(default=0, choices=[
 		(0, _("Quick")),
-		(1, _("Normal"))
+		(1, _("Normal")),
+		(2, _("Aggressive"))
 	])
 	config.softcsa.syncMode = ConfigSelection(default=0, choices=[
 		(0, _("Automatic")),
