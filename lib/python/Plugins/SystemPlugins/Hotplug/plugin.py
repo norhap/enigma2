2f9ae1ba1a2e207c621addf97ac1589530f53904
norhap <elfogondesegovia@gmail.com>
2026-09-03 03:11:18 +0200

    Hotplug: Detect USB DAB+ runtime

--- lib/python/Plugins/SystemPlugins/Hotplug/plugin.py
+++ lib/python/Plugins/SystemPlugins/Hotplug/plugin.py
@@ -28,52 +28,58 @@ def processHotplugData(self, v):
 	action = v.get("ACTION")
 	device = v.get("DEVPATH")
 	physdevpath = v.get("PHYSDEVPATH")
+	mode = v.get("mode")
 	if physdevpath == "-":
 		physdevpath = None
 	media_state = v.get("X_E2_MEDIA_STATUS")
 	global audiocd
-
 	dev = device.split('/')[-1]
-
-	if action == "add":
-		error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.addHotplugPartition(dev, physdevpath)
-	elif action == "remove":
-		harddiskmanager.removeHotplugPartition(dev)
-	elif action == "audiocdadd":
-		audiocd = True
-		media_state = "audiocd"
-		error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.addHotplugAudiocd(dev, physdevpath)
-		print("[Hotplug] AUDIO CD ADD")
-	elif action == "audiocdremove":
-		audiocd = False
-		file = []
-		# Removing the invalid playlist.e2pls If its still the audio cd's list
-		# Default setting is to save last playlist on closing Mediaplayer.
-		# If audio cd is removed after Mediaplayer was closed,
-		# the playlist remains in if no other media was played.
-		if isfile('/etc/enigma2/playlist.e2pls'):
-			with open('/etc/enigma2/playlist.e2pls', 'r') as f:
-				file = f.readline().strip()
-		if file:
-			if '.cda' in file:
-				try:
-					remove('/etc/enigma2/playlist.e2pls')
-				except OSError:
-					pass
-		harddiskmanager.removeHotplugPartition(dev)
-		print("[Hotplug] REMOVING AUDIOCD")
-	elif media_state is not None:
-		if media_state == '1':
+	if mode == 0:
+		if action == "add":
+			error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.addHotplugPartition(dev, physdevpath)
+		elif action == "remove":
 			harddiskmanager.removeHotplugPartition(dev)
-			harddiskmanager.addHotplugPartition(dev, physdevpath)
-		elif media_state == '0':
+		elif action == "audiocdadd":
+			audiocd = True
+			media_state = "audiocd"
+			error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.addHotplugAudiocd(dev, physdevpath)
+			print("[Hotplug] AUDIO CD ADD")
+		elif action == "audiocdremove":
+			audiocd = False
+			file = []
+			# Removing the invalid playlist.e2pls If its still the audio cd's list
+			# Default setting is to save last playlist on closing Mediaplayer.
+			# If audio cd is removed after Mediaplayer was closed,
+			# the playlist remains in if no other media was played.
+			if isfile('/etc/enigma2/playlist.e2pls'):
+				with open('/etc/enigma2/playlist.e2pls', 'r') as f:
+					file = f.readline().strip()
+			if file:
+				if '.cda' in file:
+					try:
+						remove('/etc/enigma2/playlist.e2pls')
+					except OSError:
+						pass
 			harddiskmanager.removeHotplugPartition(dev)
-
-	for callback in hotplugNotifier:
-		try:
-			callback(dev, action or media_state)
-		except AttributeError:
-			hotplugNotifier.remove(callback)
+			print("[Hotplug] REMOVING AUDIOCD")
+		elif media_state is not None:
+			if media_state == '1':
+				harddiskmanager.removeHotplugPartition(dev)
+				harddiskmanager.addHotplugPartition(dev, physdevpath)
+			elif media_state == '0':
+				harddiskmanager.removeHotplugPartition(dev)
+		for callback in hotplugNotifier:
+			try:
+				callback(dev, action or media_state)
+			except AttributeError:
+				hotplugNotifier.remove(callback)
+	elif mode == 1 and action == "dab-sdr-add":
+		device = v.get("DEVPATH", "").split("/")[-1]
+		for callback in hotplugNotifier[:]:
+			try:
+				callback(device, action)
+			except AttributeError:
+				hotplugNotifier.remove(callback)
 
 
 class Hotplug(Protocol):
@@ -89,8 +95,14 @@ class Hotplug(Protocol):
 
 	def connectionLost(self, reason):
 		print("[Hotplug] connection lost!")
-		data = self.received.split('\0')[:-1]
 		v = {}
+		if v.get("ACTION") != "dab-sdr-add":
+			data = self.received.split('\0')[:-1]
+			v["mode"] = 0
+		else:
+			if "\n" in self.received:
+				data = self.received.rstrip("\0\n").split("\n")
+				v["mode"] = 1
 		for x in data:
 			i = x.find('=')
 			var, val = x[:i], x[i + 1:]
